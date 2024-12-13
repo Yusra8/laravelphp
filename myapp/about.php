8de6861f3f5d59f531fa80a110a16e67f0ed031d
Yusra8 <111384288+Yusra8@users.noreply.github.com>
2024-12-13 11:12:04 +0000

    Update about.php

--- myapp/about.php
+++ myapp/about.php
@@ -12,6 +12,7 @@
 <div class="container" id="main-content">
 	<h2>About Us</h2>
 	<p> Welcome to the ABOUT PAGE! Welcome to the DevOps WRIT1 assignment. </p>
+	<p>Praesent in rhoncus nisi. Aliquam condimentum mauris in volutpat sodales. Mauris tempus aliquam pharetra. Nunc hendrerit aliquam mauris id tempor. Nam nec tellus at dui condimentum maximus. Ut congue ex a velit elementum fringilla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum vulputate eros nec lobortis condimentum. Curabitur consectetur lorem ante, ut euismod risus egestas id.</p>
 </div>
 
 <?php include("includes/footer.php");?>
