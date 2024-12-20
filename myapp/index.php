8005d6a92b52dff8953e6fc2991bf996588b742c
Yusra8 <111384288+Yusra8@users.noreply.github.com>
2024-12-20 10:26:00 +0000

    Update index.php

--- myapp/index.php
+++ myapp/index.php
@@ -11,14 +11,10 @@
 
 <div class="container" id="main-content">
 	<h2>Welcome to my website!</h2>
-	<p>Some content goes here! Let's go with the classic "lorem ipsum."</p>
-
-	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	</p>
-	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	</p>
+	<p>This website is created for DevOps WRIT1 assignment to display how DevOps cycles can be applied to an application to streamline development and deployment!</p>
+	<img src="https://media.istockphoto.com/id/1367728715/vector/devops-concept-with-infinite-loop-on-abstract-technology-background.jpg?s=612x612&w=0&k=20&c=aadwZ3TQPv31Qxd_RyCwvoNNHBT1kNiaoksHtPdfKAA=" alt"devops image">
+	<p>Please navigate to the 'about' page to find information about this website. </p>
+	<p>Please navigate to the 'Contact' page to contact us!</p>
 </div>
 
 <?php include("includes/footer.php");
