a477fbcc718fb263ebe56581fdfdf261c43574e4
Yusra8 <111384288+Yusra8@users.noreply.github.com>
2024-12-20 10:21:28 +0000

    Update about.php

--- myapp/about.php
+++ myapp/about.php
@@ -12,7 +12,8 @@
 <div class="container" id="main-content">
 	<h2>About Us</h2>
 	<p> Welcome to the ABOUT PAGE! Welcome to the DevOps WRIT1 assignment. </p>
-	<p> This page is to let you understand a bit about myself and the aim of this project. The aim of this project is to successfully apply DevOps concepts onto this program. For example, using version contorl to manage and track changes of this website - this will be achieved by using GitHub.</p>
+	<img src="https://marvel-b1-cdn.bc0a.com/f00000000236551/dt-cdn.net/wp-content/uploads/2021/07/13429_ILL_DevOpsLoop.png" alt="DevOps cycles">
+	<p> This page is to let you understand a bit about myself and the aim of this project. The aim of this project is to successfully apply DevOps concepts onto this program. For example, using version contorl to manage and track changes of this website - this will be achieved by using GitHub. Containers and images are created in Docker and Jenkins was used to run pipelines scripts. Then the Ansble playbook was implemented, Ansible is useful for setting up environments on multiple machines.</p>
 </div>
 
 <?php include("includes/footer.php");?>
